265277ce2600f7330fb9720f79d51bbe838119db
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2016-10-20 19:32:07 +0200

    feat(AndroidUtils): Demonstration of extension methods

---
+++ app/src/main/kotlin/de/bentolor/katamanager/AndroidUtils.kt
@@ -0,0 +1,29 @@
+package de.bentolor.katamanager
+
+import android.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+/**
+ * Extension properties: Direct access to all views in a [ViewGroup].
+ */
+val ViewGroup.views: List<View>
+    get() = (0..getChildCount() - 1).map { getChildAt(it) }
+
+/**
+ * Return a flat map of all contained views (recursivly) in a [ViewGroup].
+ */
+val ViewGroup.viewsRecursive: List<View>
+    get() = views.flatMap {
+        when (it) {
+            is ViewGroup -> it.viewsRecursive
+            else -> listOf(it)
+        }
+    }
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(getActivity(), message, duration).show()
+}
+
+fun ViewGroup.get(position: Int): View = getChildAt(position)
